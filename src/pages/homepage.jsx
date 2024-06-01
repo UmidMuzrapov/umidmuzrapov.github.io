5d964604d8bda3dbf80c6f4b6bd8d74fc63652f6
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2024-06-01 13:05:31 -0700

    change me.png to me jpg

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -104,7 +104,7 @@ const Homepage = () => {
 								<div className="homepage-image-container">
 									<div className="homepage-image-wrapper">
 										<img
-											src="me.png"
+											src="me.jpg"
 											alt="about"
 											className="homepage-image"
 										/>
